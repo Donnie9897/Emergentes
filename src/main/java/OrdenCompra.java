c9dae860edc02d3e608165b1978f291ade2bc3c6
nicoluv <20181669@ce.pucmm.edu.do>
2023-06-27 10:26:16 -0400

    Revert "Primera"
    This reverts commit 375b3075549eb7848e5acc4e099f806154a2aa96.

--- src/main/java/OrdenCompra.java
+++ src/main/java/OrdenCompra.java
@@ -1,17 +1,6 @@
 import javax.swing.*;
 
-public class OrdenCompra extends JFrame {
+public class OrdenCompra {
     private JPanel panelMain;
     private JLabel Fecha;
-    private JButton generarOrdenButton;
-    private JButton agregarButton;
-
-    public static void main(String[] args) {
-        OrdenCompra h=new OrdenCompra();
-        h.setContentPane(h.panelMain);
-        h.setTitle("Hello");
-        h.setSize(650,500);
-        h.setVisible(true);
-        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
 }
