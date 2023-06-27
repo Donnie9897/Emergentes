375b3075549eb7848e5acc4e099f806154a2aa96
pointersbp <50205330+pointersbp@users.noreply.github.com>
2023-06-26 22:03:41 -0400

    Primera

--- src/main/java/OrdenCompra.java
+++ src/main/java/OrdenCompra.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
 
-public class OrdenCompra {
+public class OrdenCompra extends JFrame {
     private JPanel panelMain;
     private JLabel Fecha;
+    private JButton generarOrdenButton;
+    private JButton agregarButton;
+
+    public static void main(String[] args) {
+        OrdenCompra h=new OrdenCompra();
+        h.setContentPane(h.panelMain);
+        h.setTitle("Hello");
+        h.setSize(650,500);
+        h.setVisible(true);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
